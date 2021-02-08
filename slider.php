5469ded05b5cbe4404c4c5dd274724932ef853cc
pauline-mfr <p.monfrance@codeur.online>
2021-02-08 14:42:19 +0100

    new stylesheet and slider modifications

--- slider.php
+++ slider.php
@@ -1,139 +1,96 @@
 <!doctype html>
 <html>
 <head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA_Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, intial-scale=1.0">
-
-
-<!--stylesheet------------->
-<link rel="stylesheet" type="text/css" href="css/stylerea.css">
-<!--light-slider.css------------->
-<link rel="stylesheet" type="text/css" href="css/lightslider.css">
-<!--Jquery-------------------->
-<script type="text/javascript" src="js/Jquery.js"></script>
-<!--lightslider.js--------------->
-<script type="text/javascript" src="js/lightslider.js"></script>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA_Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, intial-scale=1.0">
+	<!--light-slider.css------------->
+	<link rel="stylesheet" type="text/css" href="css/lightslider.css">
+	<!--lightslider.js--------------->
+	<script type="text/javascript" src="js/lightslider.js"></script>
 </head>
-
 <body>
 	<section class="slider">
-	<ul id="autoWidth" class="cs-hidden">
-	<!--1------------------------------------>
-  <li class="item-a">
-<!--box-slider--------------->
-	<div class="box">
-	<!--img-box---------->
-	<div class="slide-img">
-	<img src="img/1.png" alt="1">
-	<!--overlayer---------->
-	<div class="overlay1">
-	<!--buy-btn------>
-
-	<h2 class="buy-btn">Crinkle Top Brownies</h2><br>
-<p class="desc2">Lorem ipsum dolor sit amet, consetetur<br> sadipscing elitr….</p>
-	</div>
-	</div>
-
-
-	</div>
-</li>
-
-	<!--2------------------------------------>
-  <li class="item-b">
-<!--box-slider--------------->
-	<div class="box">
-	<!--img-box---------->
-	<div class="slide-img">
-	<img src="img/h4wl2hhg.png" alt="2">
-	<!--overlayer---------->
-	<div class="overlay1">
-		<h2 class="buy-btn">Crinkle Top Brownies</h2><br>
-		<p class="desc2">Lorem ipsum dolor sit amet, consetetur<br> sadipscing elitr….</p>
-	</div>
-	</div>
-
-	</div>
-</li>
-	<!--3------------------------------------>
-  <li class="item-c">
-<!--box-slider--------------->
-	<div class="box">
-	<!--img-box---------->
-	<div class="slide-img">
-	<img src="img/food-photographer-jennifer-pallian-306915-unsplash.png" alt="3">
-	<!--overlayer---------->
-	<div class="overlay1">
-		<h2 class="buy-btn">Crinkle Top Brownies</h2><br>
-		<p class="desc2">Lorem ipsum dolor sit amet, consetetur<br> sadipscing elitr….</p>
-	</div>
-	</div>
-	</div>
-</li>
-	<!--4------------------------------------>
-  <li class="item-d">
-<!--box-slider--------------->
-	<div class="box">
-	<!--img-box---------->
-	<div class="slide-img">
-	<img src="img/gianna-trewavas-641234-unsplash.png" alt="4">
-	<!--overlayer---------->
-	<div class="overlay1">
-		<h2 class="buy-btn">Crinkle Top Brownies</h2><br>
-		<p class="desc2">Lorem ipsum dolor sit amet, consetetur<br> sadipscing elitr….</p>
-	</div>
-	</div>
-	</div>
-</li>
-	<!--5------------------------------------>
-  <li class="item-e">
-<!--box-slider--------------->
-	<div class="box">
-	<!--img-box---------->
-	<div class="slide-img">
-	<img src="img/h4wl2hhg.png" alt="5">
-	<!--overlayer---------->
-	<div class="overlay1">
-		<h2 class="buy-btn">Crinkle Top Brownies</h2><br>
-		<p class="desc2">Lorem ipsum dolor sit amet, consetetur<br> sadipscing elitr….</p>
-	</div>
-	</div>
-	</div>
-</li>
-	<!--6------------------------------------>
-  <li class="item-f">
-<!--box-slider--------------->
-	<div class="box">
-	<!--img-box---------->
-	<div class="slide-img">
-	<img src="img/1.png" alt="6">
-	<!--overlayer---------->
-	<div class="overlay1">
-		<h2 class="buy-btn">Crinkle Top Brownies</h2><br>
-		<p class="desc2">Lorem ipsum dolor sit amet, consetetur<br> sadipscing elitr….</p>
-	</div>
-	</div>
-	</div>
-</li>
-	<!--7------------------------------------>
-  <li class="item-g">
-<!--box-slider--------------->
-	<div class="box">
-	<!--img-box---------->
-	<div class="slide-img">
-	<img src="img/h4wl2hhg.png" alt="7">
-	<!--overlayer---------->
-	<div class="overlay1">
-		<h2 class="buy-btn">Crinkle Top Brownies</h2><br>
-		<p class="desc2">Lorem ipsum dolor sit amet, consetetur<br> sadipscing elitr….</p>
-	</div>
-	</div>
-	</div>
-</li>
-</ul>
+		<ul id="autoWidth" class="cs-hidden">
+			<li>
+				<div class="box">
+					<div class="slide-img">
+						<img src="img/1.png" alt="1">
+						<div class="overlay1">
+							<h2 class="buy-btn">Crinkle Top Brownies</h2><br>
+							<p class="desc2">Lorem ipsum dolor sit amet, consetetur<br> sadipscing elitr….</p>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="item-b">
+				<div class="box">
+					<div class="slide-img">
+						<img src="img/h4wl2hhg.png" alt="2">
+						<div class="overlay1">
+							<h2 class="buy-btn">Crinkle Top Brownies</h2><br>
+							<p class="desc2">Lorem ipsum dolor sit amet, consetetur<br> sadipscing elitr….</p>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="item-c">
+				<div class="box">
+					<div class="slide-img">
+						<img src="img/food-photographer-jennifer-pallian-306915-unsplash.png" alt="3">
+						<div class="overlay1">
+							<h2 class="buy-btn">Crinkle Top Brownies</h2><br>
+							<p class="desc2">Lorem ipsum dolor sit amet, consetetur<br> sadipscing elitr….</p>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="item-d">
+				<div class="box">
+					<div class="slide-img">
+						<img src="img/gianna-trewavas-641234-unsplash.png" alt="4">
+						<div class="overlay1">
+							<h2 class="buy-btn">Crinkle Top Brownies</h2><br>
+							<p class="desc2">Lorem ipsum dolor sit amet, consetetur<br> sadipscing elitr….</p>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="item-e">
+				<div class="box">
+					<div class="slide-img">
+						<img src="img/h4wl2hhg.png" alt="5">
+						<div class="overlay1">
+							<h2 class="buy-btn">Crinkle Top Brownies</h2><br>
+							<p class="desc2">Lorem ipsum dolor sit amet, consetetur<br> sadipscing elitr….</p>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="item-f">
+				<div class="box">
+					<div class="slide-img">
+						<img src="img/1.png" alt="6">
+						<div class="overlay1">
+							<h2 class="buy-btn">Crinkle Top Brownies</h2><br>
+							<p class="desc2">Lorem ipsum dolor sit amet, consetetur<br> sadipscing elitr….</p>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="item-g">
+				<div class="box">
+					<div class="slide-img">
+						<img src="img/h4wl2hhg.png" alt="7">
+						<div class="overlay1">
+							<h2 class="buy-btn">Crinkle Top Brownies</h2><br>
+							<p class="desc2">Lorem ipsum dolor sit amet, consetetur<br> sadipscing elitr….</p>
+						</div>
+					</div>
+				</div>
+			</li>
+		</ul>
 	</section>
-
-<!--script-link----------->
-<script type="text/javascript" src="js/script.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
 </body>
 </html>
